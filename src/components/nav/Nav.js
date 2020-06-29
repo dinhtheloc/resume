eeb1ba78fdf91d7301509eb71276893ec70f64c5
dinh the loc <locdt2@vng.com.vn>
2020-06-29 18:30:55 +0700

    update avatar

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -38,7 +38,7 @@ function Nav() {
                 <div>
                     <a href="#" className="avatar rounded-circle avatar-xl">
                         <img alt="Image placeholder"
-                            src="https://instagram.fhan2-3.fna.fbcdn.net/v/t51.2885-19/s320x320/96292822_244805370090236_7062501899211112448_n.jpg?_nc_ht=instagram.fhan2-3.fna.fbcdn.net&_nc_ohc=kAlED3kvz-4AX9aKVge&oh=2ecd67c84b4bb2ff9e5b8ec6c9526a56&oe=5EF797E4"
+                            src="/img/avatar.jpg"
                         />
                     </a>
                     <div className="mt-4">
